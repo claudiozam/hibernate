0c36fa5f704d6ce6676777d4058096456e3ab704
Claudio Zamoszczyk <claudio.zamoszczyk@gmail.com>
2023-04-04 12:34:19 -0300

    Ejemplo de ABM

--- src/main/java/edu/curso/java/hibernate/Principal.java
+++ src/main/java/edu/curso/java/hibernate/Principal.java
@@ -1,7 +1,10 @@
 package edu.curso.java.hibernate;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class Principal {
 
@@ -12,16 +15,37 @@ public class Principal {
 		
 		Transaction transaction = session.beginTransaction();
 		
-		//Producto producto1 = new Producto("Ejemplo producto 1", 5000.0);
 		
+		//ALTA
+		//Producto producto1 = new Producto("Ejemplo producto 4", 5000.0);
 		//session.save(producto1);
-
 		//System.out.println("El id generado del producto es: " + producto1.getId());
 
-		Long idBuscar = 1L;
-		Producto productoRecuperado = session.get(Producto.class, idBuscar);
-		System.out.println(productoRecuperado);
+		//CONSULTA POR ID
+		//Long idBuscar = 1L;
+		//Producto productoRecuperado = session.get(Producto.class, idBuscar);
+		//System.out.println(productoRecuperado);
+
 		
+		//UPDATE
+		//Long idBuscar = 1L;
+		//Producto productoRecuperado = session.get(Producto.class, idBuscar);
+		//System.out.println(productoRecuperado);
+		//productoRecuperado.setNombre("Ejemplo producto 1 con update");
+		//session.update(productoRecuperado);
+		
+		//Long idBuscar = 1L;
+		//Producto productoRecuperado = session.get(Producto.class, idBuscar);
+		//System.out.println(productoRecuperado);
+		//productoRecuperado.setNombre("Ejemplo producto 1 con update");
+		//session.delete(productoRecuperado);
+		
+		Query<Producto> consultaProductos = session.createQuery("from Producto as p order by p.nombre", Producto.class);
+		List<Producto> productos = consultaProductos.list();
+		
+		for (Producto p : productos) {
+			System.out.println(p);
+		}
 		
 		transaction.commit();
 		session.close();
