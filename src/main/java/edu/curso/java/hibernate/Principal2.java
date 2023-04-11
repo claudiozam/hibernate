717a8c34dfbc3f72aa0b3a43f5b719e556d27d77
Claudio Zamoszczyk <claudio.zamoszczyk@gmail.com>
2023-04-11 12:41:57 -0300

    ejemplo simple de query con parametros

---
+++ src/main/java/edu/curso/java/hibernate/Principal2.java
@@ -0,0 +1,35 @@
+package edu.curso.java.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		
+		Transaction transaction = session.beginTransaction();
+		
+		Query<Producto> consultaProductos = session.createQuery("from Producto as p where p.precio > :precio order by p.nombre ", Producto.class);
+		consultaProductos.setParameter("precio", 2000.0);
+	
+		List<Producto> productos = consultaProductos.list();
+		
+		for (Producto p : productos) {
+			System.out.println(p);
+		}
+		
+		transaction.commit();
+		session.close();
+		
+		HibernateUtil.close();
+		
+		
+	}
+
+}
