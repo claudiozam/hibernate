4bbce79c46fa3767de8d16c71222770d2c6ee584
Claudio Zamoszczyk <claudio.zamoszczyk@gmail.com>
2023-04-11 12:00:38 -0300

    ejemplo de one to many alta

--- src/main/java/edu/curso/java/hibernate/Principal.java
+++ src/main/java/edu/curso/java/hibernate/Principal.java
@@ -19,22 +19,39 @@ public class Principal {
 		//CategoriaProducto categoriaProducto = new CategoriaProducto();
 		//categoriaProducto.setNombre("General");
 		//session.save(categoriaProducto);
-		//Long idCategoria = 6L;
-		//CategoriaProducto categoriaProducto = session.get(CategoriaProducto.class, idCategoria);
-		//ALTA
-		//Producto producto1 = new Producto("Ejemplo producto 4", 5000.0);
-		//producto1.setCategoriaProducto(categoriaProducto);
-		//session.save(producto1);
-		//System.out.println("El id generado del producto es: " + producto1.getId());
+		
+		
+		Deposito deposito1 = new Deposito();
+		deposito1.setNombre("Depo1");
 
+		Deposito deposito2 = new Deposito();
+		deposito2.setNombre("Depo2");
+
+		session.save(deposito1);
+		session.save(deposito2);
+
+		Long idCategoria = 6L;
+		CategoriaProducto categoriaProducto = session.get(CategoriaProducto.class, idCategoria);
+		
+		//ALTA
+		Producto producto1 = new Producto("Ejemplo producto 4", 5000.0);
+		producto1.setCategoriaProducto(categoriaProducto);
+		producto1.getDepositos().add(deposito1);
+		producto1.getDepositos().add(deposito2);
+		session.save(producto1);
+		System.out.println("El id generado del producto es: " + producto1.getId());
+
+		
 		//CONSULTA POR ID
 		Long idBuscar = 10L;
 		Producto productoRecuperado = session.get(Producto.class, idBuscar);
 		System.out.println(productoRecuperado);
-		CategoriaProducto categoriaProducto = productoRecuperado.getCategoriaProducto();
-		System.out.println(categoriaProducto);
+		//CategoriaProducto categoriaProducto = productoRecuperado.getCategoriaProducto();
+		//System.out.println(categoriaProducto);
 
-		
+		for(Deposito deposito : productoRecuperado.getDepositos()) {
+			System.out.println(deposito);
+		}
 		//UPDATE
 		//Long idBuscar = 1L;
 		//Producto productoRecuperado = session.get(Producto.class, idBuscar);
